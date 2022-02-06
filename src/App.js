275a2aabc2ae5bada3cdb5fc44bc16e5f64ebc3f
AllanCicceri <allan.cicceri@gmail.com>
2022-02-06 16:18:28 -0300

    add: Background-image separeted in 3 sections (for regions)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-screen">
+      <div className="bg-[url('/public/pokeworldmap.webp')] w-full h-full bg-no-repeat bg-center bg-fixed flex flex-col">
+        <div className='h-full bg-white opacity-50 hover:opacity-0 transition-opacity ease-in-out delay-800'></div>
+        <div className='h-full bg-white opacity-50 hover:opacity-0 transition-opacity ease-in-out delay-800'></div>
+        <div className='h-full bg-white opacity-50 hover:opacity-0 transition-opacity ease-in-out delay-800'></div>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [    "./src/**/*.{js,jsx,ts,tsx}",  ],
+  theme: {
+    extend: {
+      backgroundImage: 
+      {
+        'world-map': "url('/public/pokemonworldmap1.webp')",
+        'world-map': "url('/public/pokemonworldmap2.webp')",
+        'world-map': "url('/public/pokemonworldmap3.webp')",
+      }
+    },
+  },
+  plugins: [],
+}
