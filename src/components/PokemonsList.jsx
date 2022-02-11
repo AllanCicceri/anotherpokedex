7ce1fa38242a94196ae07de5173c501f6dc23230
AllanCicceri <allan.cicceri@gmail.com>
2022-02-11 19:25:34 -0300

    Add: fetching pokemon sprites

--- src/components/PokemonsList.jsx
+++ src/components/PokemonsList.jsx
@@ -1,22 +1,45 @@
 import {useState, useEffect} from 'react'
+import Pokemon from './Pokemon'
 
 function PokemonsList(){
-    const [pokemonsList, setPokemonsList] = useState([])
+    const [pokemonsList, setPokemonsList] = useState(null)
 
     useEffect(() => {
         const getPokemons = async () => {
             const apiResponse = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0')
             const data = await apiResponse.json()
-            const listOfPokemons = Array.from(data.results)
-            // console.log(apiResponse.next)//next page
-            console.log(listOfPokemons)
+            const list = Array.from(data.results)
+
+            let listOfPokemons = await Promise.all(
+                list.map(item => {
+                    const poke = fetch(item.url).then(resp => resp.json()).then(json => {
+                        return {id:json.id, name: item.name, image:json.sprites.front_default}                     
+                    })
+                    return poke
+                })
+
+            )
+            
+            setPokemonsList(listOfPokemons)
+            
         }
 
         getPokemons()
     },[])
 
+    const renderListOfPokemons = () => {
+        return pokemonsList !== null && pokemonsList.map(item => (
+            <div>
+                <img src={item.image} alt="" />
+                <span>{`#${item.id} ${item.name}`}</span>
+            </div>
+        ))
+    }
+
     return (
-        <div className="h-full" >poke</div>
+        <div className="h-full" >
+            {pokemonsList !== null && renderListOfPokemons()}
+        </div>
     )
 }
 
