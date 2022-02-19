d80f624a85fba17aa72299e3227c1f0ecd10a968
AllanCicceri <allan.cicceri@gmail.com>
2022-02-18 23:31:22 -0300

    Add: types icons (needs to color them)

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import { useLocation } from "react-router-dom"
-import {camelize} from '../assets/js/util'
+import { camelize } from '../assets/js/util'
 
 function Pokemon({ props }) {
     const location = useLocation()
@@ -8,20 +8,32 @@ function Pokemon({ props }) {
 
     const [pokemon, setPokemon] = useState(null)
 
-    
+
     useEffect(async () => {
-        async function getPokemonInfo(){
+        async function getPokemonInfo() {
             const response = await fetch(url)
             const data = await response.json()
-            const respImage = await (await fetch(data.sprites.other["official-artwork"].front_default)).url
-            
+            const respImage = (await fetch(data.sprites.other["official-artwork"].front_default)).url
+            let typesRes = []
+            data.types.map(item => {
+                typesRes.push(item.type.name)
+            })
+
+            let typesImg = []
+            typesRes.map( item => {
+                typesImg.push(`/types/${item}.svg`)
+            }
+            )
+
+
             const pokemon = {
                 name: camelize(name),
                 img: respImage,
                 height: data.height,
-                weight: data.weight
+                weight: data.weight,
+                types: typesImg
             }
-    
+
             return pokemon
         }
 
@@ -29,13 +41,21 @@ function Pokemon({ props }) {
         setPokemon(pokemon)
     }, [])
 
-    function renderPokemon(){
-        return(
+
+
+    function renderPokemon() {
+        return (
             <div className="flex items-center justify-center">
                 <div className="pokemonInfo">
                     <img src={pokemon.img} alt="" className="h-98 w-98" />
                     <div className="font-bold text-7xl">{pokemon.name}</div>
                     <div>Height: {pokemon.height}, Weight: {pokemon.weight}</div>
+                    <div className="flex">
+                        {pokemon.types.map(item => (
+                            <img src={item} alt="icons of pokemon type" className="bg-black w-10 h-10 "/>
+                        ))}
+                    </div>
+
                 </div>
                 <div className="pokemonMoves">
 
@@ -45,7 +65,7 @@ function Pokemon({ props }) {
     }
 
     return (
-            pokemon !== null && renderPokemon()
+        pokemon !== null && renderPokemon()
     )
 }
 
